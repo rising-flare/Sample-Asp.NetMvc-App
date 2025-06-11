68ec4162368e0b171e1786e102f7dfc1bc193585
SharafatKarim <sharafat2004@gmail.com>
2025-06-11 09:14:02 +0600

    Add project files.

---
+++ SampleApp/Data/SampleAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SampleApp.Models;
+
+namespace SampleApp.Data
+{
+    public class SampleAppContext : DbContext
+    {
+        public SampleAppContext (DbContextOptions<SampleAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SampleApp.Models.Movie> Movie { get; set; } = default!;
+    }
+}
---
+++ SampleApp/Models/MovieGenreViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using SampleApp.Models;
+using System.Collections.Generic;
+
+namespace SampleApp.Models;
+
+public class MovieGenreViewModel
+{
+    public List<Movie>? Movies { get; set; }
+    public SelectList? Genres { get; set; }
+    public string? MovieGenre { get; set; }
+    public string? SearchString { get; set; }
+}
